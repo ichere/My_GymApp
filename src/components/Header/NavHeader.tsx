94304430f5c13d4a1b25eae30b8693d465eda87a
unknown <izuagbalachere@gmail.com>
2024-03-06 22:52:32 +0100

    Navbar fix 1

--- src/components/Header/NavHeader.tsx
+++ src/components/Header/NavHeader.tsx
@@ -1,5 +1,8 @@
 import { Box, Flex, Link, Text } from "@chakra-ui/react"
-import { LANDING_PARAGRAPH_FONT } from "../../constants/appConstants"
+import { LANDING_PARAGRAPH_FONT, RESPONSIVE_BUTTON_ICON_SIZE } from "../../constants/appConstants"
+import { BsInstagram, BsTiktok } from "react-icons/bs"
+import { COLORS } from "../../constants/colors"
+import { FaFacebook } from "react-icons/fa"
 
 
 export const NavHeader = () => {
@@ -21,18 +24,28 @@ export const NavHeader = () => {
           <Text>LOGO</Text>
         </Box>
         <Flex
-          flexDirection={['column', 'column', 'row']}
           justifyContent={'space-around'}
         >
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Home</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Training</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Facilities</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Amenities</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Join</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Blog</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Gallery</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>Merch</Link>
-          <Link fontSize={LANDING_PARAGRAPH_FONT}>About</Link>
+          <Flex
+            flexDirection={['column', 'column', 'row']}
+            justifyContent={'space-between'}
+            gap={'1rem'}
+          >
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Home</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Training</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Facilities</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Amenities</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Join</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Blog</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Gallery</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>Merch</Link>
+            <Link fontSize={LANDING_PARAGRAPH_FONT}>About</Link>
+          </Flex>
+          <Flex>
+            <BsTiktok color={COLORS.instagramBlue} />
+            <BsInstagram color={COLORS.instagramBlue} />
+            <FaFacebook color={COLORS.facebookBlue} />
+          </Flex>
         </Flex>
       </Flex>
     </>

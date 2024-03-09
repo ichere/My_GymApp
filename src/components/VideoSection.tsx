2e4f41701f36220ac5da51e8d58c5094e03d42b8
unknown <izuagbalachere@gmail.com>
2024-03-09 19:38:39 +0100

    tested

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -2,10 +2,15 @@ import { Flex } from '@chakra-ui/react'
 
 export const VideoSection = (): JSX.Element => {
   return (
-    <Flex position={'fixed'}  minH={'100%'} minW={'100%'} right={0} bottom={0}>
-      <video className='video' autoPlay muted loop>
+    <>
+      <Flex position={'fixed'}  minH={'100%'} minW={'100%'} right={0} bottom={0}>
+        <video height={'100%'} width={'100%'} autoPlay muted loop>
+          <source src="https://www.youtube.com/shorts/ttuKeS5k4Uo" type="video/mp4"/>
+        </video>
+      </Flex>
+      {/* <video height={'100%'} width={'100%'} autoPlay muted loop>
         <source src="https://www.youtube.com/shorts/ttuKeS5k4Uo" type="video/mp4"/>
-      </video>
-    </Flex>
+      </video> */}
+    </>
   )
 }
